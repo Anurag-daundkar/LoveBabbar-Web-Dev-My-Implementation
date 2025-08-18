f7d9223ffcaa0bfd8710cf2108f6b1365bf0cb59
anuragdaundkar <anuragdaundkar@gmail.com>
2025-08-18 22:39:21 +0530

    Add files via upload

---
+++ Lecture40/index.js
@@ -0,0 +1,46 @@
+// All Types to create variables
+
+var c = 'Anurag';
+console.log(c);
+
+let d = 'Santosh';
+console.log(d);
+
+const a = 'Daundkar';
+console.log(a);
+
+// -----------------------------------------------------
+
+// Functions
+
+function Multiply(num)
+{
+    let ans = num*num;
+    return ans;
+}
+
+let answer = Multiply(10);
+console.log(answer);
+
+
+// -----------------------------------------------------
+
+// Different Datatypes
+
+let number = 10;
+console.log(typeof(number), number);
+
+let string = 'Krishna';
+console.log(typeof(string), string);
+
+let boolean = true;
+console.log(typeof(boolean), boolean);
+
+let undefine;
+console.log(typeof(undefine), undefine);
+
+let NULL = null;
+console.log(typeof(NULL), NULL);
+
+let BigInt = 100000000000000000000000000000000000000000;
+console.log(typeof(BigInt), BigInt);
